a7cc5998cc2f3616dc4216c6336befc05abd591d
santiago <bassilsantiago@gmail.com>
2023-07-22 11:42:55 -0500

    Feat: renders message contents

---
+++ src/lib/ChatMessage.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let content;
+    export let isAi 
+</script>
+
+<div>
+    {#if isAi}
+        <p>Ai message: {content}</p>
+        {:else}
+          <p>Your message: {content}</p> 
+    {/if}
+</div>
