7ccde5ab75efba0bebf5dd6ba9542845fa492629
santiago <bassilsantiago@gmail.com>
2023-07-21 19:08:36 -0500

    Define chat here

---
+++ src/lib/Chat.svelte
@@ -0,0 +1,9 @@
+<script>
+    function handleSignOut() {
+        localStorage.removeItem("jwtToken")
+        window.location.reload()
+    }
+</script>
+
+<h1>Chat</h1>
+<button on:click={handleSignOut}>Sing out</button>
