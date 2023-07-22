dabac932b8dec57e2ae4f48f28ddcb754fee1fe1
santiago <bassilsantiago@gmail.com>
2023-07-21 19:10:17 -0500

    Feat: displays components based on user auth

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  // @ts-nocheck
+
+  import SideBar from "./lib/SideBar.svelte";
+  import Login from "./lib/Login.svelte";
+  import { Router, Link, Route } from "svelte-routing";
+  import Chat from "./lib/Chat.svelte";
+
+  const token = localStorage.getItem("jwtToken");
+  let url = "";
+  $: {
+    if (token) {
+      url = "/chat";
+    } else {
+      url = "/";
+    }
+  }
+</script>
+
+<Router {url}>
+  <Route path="/" component={Login} />
+
+  <Route path="/chat" component={Chat} />
+</Router>
+
+<style>
+</style>
