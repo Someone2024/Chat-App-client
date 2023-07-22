de7f1f5c0afa62badf26cf6bbe0c2ed6d419c3fd
santiago <bassilsantiago@gmail.com>
2023-07-22 11:41:07 -0500

    Feat: renders chat messages

--- src/lib/Chat.svelte
+++ src/lib/Chat.svelte
@@ -1,9 +1,21 @@
 <script>
+
+    import ChatMessage from "./ChatMessage.svelte";
+
     function handleSignOut() {
         localStorage.removeItem("jwtToken")
         window.location.reload()
+    }
+
+    async function handleChat() {
+        
     }
 </script>
 
 <h1>Chat</h1>
 <button on:click={handleSignOut}>Sing out</button>
+<div>
+    {#each messages as message }
+        <ChatMessage isAi={message.isAi} content={message.content}/>
+    {/each}
+</div>
