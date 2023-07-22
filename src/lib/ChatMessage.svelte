8dcd6adbe16283b6b6e4926d56c05c670fd4ff98
santiago <bassilsantiago@gmail.com>
2023-07-22 14:04:40 -0500

    Feat: Renders message based on role

--- src/lib/ChatMessage.svelte
+++ src/lib/ChatMessage.svelte
@@ -1,10 +1,10 @@
 <script>
     export let content;
-    export let isAi 
+    export let role = "not provided"
 </script>
 
 <div>
-    {#if isAi}
+    {#if role === "assistant"}
         <p>Ai message: {content}</p>
         {:else}
           <p>Your message: {content}</p> 
