f83780d9c4ff71d30dc393ad9fcf1e2b930b01b7
santiago <bassilsantiago@gmail.com>
2023-07-22 23:02:14 -0500

    Feat: Log out with log out button

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -1,5 +1,6 @@
 <script>
   import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+  import {handleSignOut} from "./api/HandleChat"
   let spanClass = 'flex-1 ml-3 whitespace-nowrap';
 </script>
 
@@ -38,7 +39,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" /></svg>
         </svelte:fragment>
       </SidebarItem>
-      <SidebarItem label="Sign In">
+      <SidebarItem on:click={handleSignOut} label="Log out">
         <svelte:fragment slot="icon">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" /></svg>
         </svelte:fragment>
